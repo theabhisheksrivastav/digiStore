b02dd379a7043478ade7416c4859a65a492e4637
abhisheksrivastavofficial <abhisheksrivastavofficial@gmail.com>
2024-07-28 17:06:40 +0530

    chore: Update PurchasePage to fetch product data and handle availability check

--- src/app/(client)/products/[id]/purchase/page.tsx
+++ src/app/(client)/products/[id]/purchase/page.tsx
@@ -3,8 +3,12 @@ import { notFound } from "next/navigation";
 import { CheckoutForm } from "./_components/CheckoutForm";
 
 export default async function PurchasePage({params: {id}}: {params: {id: string}}) {
-    const product = await db.product.findUnique({where: {id}});
-    if (!product) {
+    const product = await db.product.findUnique({
+        where: { id },
+        select: { id: true, name: true, priceInPaise: true, description: true, isAvailable: true, imagePath: true },
+    });
+    console.log(product);
+    if (!product || !product.isAvailable) {
         return notFound();
     }
 
